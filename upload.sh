ab0301345961d0498d42e5678a28d9bf8ddd3a8b
Ajdin <ajdin.m@gmail.com>
2017-01-13 16:49:42 +0100

    modernied

--- upload.sh
+++ upload.sh
@@ -8,6 +8,4 @@ scrot -s "shot.png"
 uploadImage "shot.png"
 echo $url | awk -F ".png" '{print "http://pokit.org/get/img/" $1 ".png" }' | xclip -selection c
 rm "shot.png"
-notify-send "Done"
-sleep 0.2
-killall notify-osd
+notify-send "Done" -t 20
